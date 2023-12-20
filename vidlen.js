069472deb8e25ed638806c878105c2039fccfde2
Parker-Parker <pagrant@wpi.edu>
2023-12-20 15:40:43 -0500

    fixed manifest errors and some added some debug to script

--- vidlen.js
+++ vidlen.js
@@ -1,14 +1,15 @@
 // Select the node that will be observed for mutations
 // const targetNode = document.getElementById("some-id");
-const targetNode = document.head.title;
+const targetNode = document.title;
 
 // Options for the observer (which mutations to observe)
-const config = { childList: true };
+const config = { attributes: true, childList: true, characterData: true };
 
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   console.log(`glorp`);
   console.log(document.head.title);
+  console.log(document.title);
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
       console.log("A child node has been added or removed.");
@@ -23,13 +24,20 @@ const callback = (mutationList, observer) => {
 // Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
 
+
+
+console.log(targetNode);
+console.log(config);
+console.log(callback);
+console.log(document.title);
 // Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+//observer.observe(targetNode, config);
 
 
-document.head.title = "yeet";
-document.head.title = "osjdhaskjhdkljsadkljhasdkjnsadkjhdskjh";
-document.head.title = "test";
-
+document.title = "yeet";
+document.title = "osjdhaskjhdkljsadkljhasdkjnsadkjhdskjh";
+document.title = "test";
+console.log(document.getElementsByTagName("video")[0].duration)
+document.title = document.getElementsByTagName("video")[0].duration;
 console.log(`glorp`);
 
