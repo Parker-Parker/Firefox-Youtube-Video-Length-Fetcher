173ed5e242d581ec4254905356cb7f97d80d1dfa
Parker-Parker <pagrant@wpi.edu>
2023-12-20 16:54:57 -0500

    cleanup

--- vidlen.js
+++ vidlen.js
@@ -1,5 +1,4 @@
 // Select the node that will be observed for mutations
-// const targetNode = document.getElementById("some-id");
 const targetNode = document.querySelector('title');;
 
 // Options for the observer (which mutations to observe)
@@ -36,22 +35,10 @@ const finalTitle = makeDurationString(videoLengthSeconds)+initialTitle;
 
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
-  console.log(document.title);
-                                //   for (const mutation of mutationList) {
-                                //     if (mutation.type === "childList") {
-                                //       console.log("A child node has been added or removed.");
-                                //     } else if (mutation.type === "attributes") {
-                                //       console.log(`The ${mutation.attributeName} attribute was modified.`);
-                                //     } else {
-                                //         console.log(`WEENIE`);
-                                //     }
-                                //   }
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
-      console.log("Title has been changed to: "+document.title+" ... Correcting");
       if(!document.title.startsWith("_"))  {
         document.title = finalTitle;
-        console.log("Corrected to: "+document.title);
       }
     } 
   }
@@ -62,19 +49,7 @@ const observer = new MutationObserver(callback);
 
 
 
-console.log(targetNode);
-console.log(config);
-console.log(callback);
-console.log(document.title);
 // Start observing the target node for configured mutations
 observer.observe(targetNode, config);
 
-
-document.title = "yeet";
-document.title = "osjdhaskjhdkljsadkljhasdkjnsadkjhdskjh";
-document.title = "test";
-console.log(document.getElementsByTagName("video")[0].duration)
-document.title = Math.round(document.getElementsByTagName("video")[0].duration);
-console.log(document.title)
-console.log(`glorp`);
 
